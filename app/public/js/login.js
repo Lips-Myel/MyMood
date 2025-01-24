6d06076b5df0ffd4f6be4b6cb6ae7d90cb90891a
Lips-Myel <ludovic.picaud@institutsolacroup.com>
2025-01-24 17:22:34 +0100

    login fonctionnel

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -3,14 +3,14 @@ const loginForm = document.querySelector('#login-form');
 loginForm.addEventListener('submit', event => {
     event.preventDefault();
 
-    const loginUsername = document.querySelector('#login-username').value;
+    const loginEmail = document.querySelector('#login-username').value;
     const loginPassword = document.querySelector('#login-password').value;
 
-    authentificate(loginUsername, loginPassword);
+    authentificate(loginEmail, loginPassword);
 });
 
-function authentificate(username, password) {
-    console.log(username);
+function authentificate(email, password) {
+    console.log(email);
     console.log(password);
 
     const loginParams = {
@@ -18,18 +18,21 @@ function authentificate(username, password) {
         headers: {
             'Content-Type': 'application/json', // Type de contenu
         },
-        body: JSON.stringify({ username, password }), // Données envoyées au serveur
+        body: JSON.stringify({ email, password  }), // Données envoyées au serveur
         credentials: 'include'  // Ajoutez cela si vous envoyez des cookies
     };
 
-    const apiUrl = 'http://localhost/api/login';
+    const apiUrl = 'https://localhost:443/api/login';
     fetch(apiUrl, loginParams)
         .then(response => {
             if (!response.ok) {
-                throw new Error(HTTP `error! status: ${response.status}`);
+                throw new Error(`HTTP error! status: ${response.status}`);
             }
             return response.json();
         })
-        .then(data => console.log('Réponse du serveur :', data))
+        .then(data => {
+            console.log('Réponse du serveur :', data);
+            // Ajoutez ici la logique pour gérer la réponse, par exemple la redirection
+        })
         .catch(error => console.error('Erreur :', error));
 }
